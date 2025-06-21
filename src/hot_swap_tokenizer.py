7f8fcb233cbfc0a4a2eca4017fd861e61b702f06
David Heineman <david@davidheineman.com>
2025-06-20 21:59:29 -0700

    fix loader

--- src/hot_swap_tokenizer.py
+++ src/hot_swap_tokenizer.py
@@ -1,16 +1,17 @@
+from pathlib import Path
 from transformers import AutoModelForCausalLM
 from tokenizers import Tokenizer
 import torch.nn as nn
 
 from constants import DATA_DIR
 
-def hot_swap_tokenizer(model_name: str, tokenizer_path: str):
+def hot_swap_tokenizer(model_name: str, tokenizer_path: Path):
     print("Loading original model...")
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cpu") # we only need cpu
     orig_embed_dim = model.get_input_embeddings().embedding_dim
     
     print("Loading new tokenizer...")
-    new_tokenizer = Tokenizer.from_file(tokenizer_path)
+    new_tokenizer = Tokenizer.from_file(str(tokenizer_path))
     new_vocab_size = new_tokenizer.get_vocab_size()
     
     print("Creating new embedding layer...")
@@ -30,13 +31,13 @@ def hot_swap_tokenizer(model_name: str, tokenizer_path: str):
 
 if __name__ == "__main__":
     # Example usage
-    tokenizer = "s1"
+    tokenizer_name = "s1"
     model_name = "allenai/OLMo-2-1124-7B"
-    tokenizer_path = DATA_DIR / f"{tokenizer}.json"
+    tokenizer_path = DATA_DIR / f"{tokenizer_name}.json"
     
     model, tokenizer = hot_swap_tokenizer(model_name, tokenizer_path)
 
     # Save model with new tokenizer
-    output_path = DATA_DIR / f"{model_name.split('/')[-1]}-{tokenizer}_tokenizer"
+    output_path = DATA_DIR / f"{model_name.split('/')[-1]}-{tokenizer_name}_tokenizer"
     print(f"Saving model to {output_path}...")
     model.save_pretrained(output_path)
